470560d3a0346a942fa3419927c5a79c7525f34f
hassanchakkour <hassan.chakkour@hotmail.com>
2023-03-17 22:02:29 +0200

    user registration with backEnd

--- covid-app/src/Components/User-Register/UserRegister.js
+++ covid-app/src/Components/User-Register/UserRegister.js
@@ -2,11 +2,52 @@ import React from 'react'
 import { useState } from 'react'
 import Button from '../Button/Button'
 import './UserRegister.css'
-import {AiOutlineUser} from 'react-icons/ai'
 
 const UserRegister = () => {
+
+const [confirmPass, setConfirmPass] = useState('');
+ const [errconfirm, setErrconfirm] = useState('')
+const [errorPass, setErrorPass] = useState('');
+const [errorusername, setErrorusername] = useState('');
+
+    const handleSubmit = async (e) => { 
+        e.preventDefault()
+      const  result = await fetch('http://localhost:3600/Register', {
+            method: 'POST',
+            headers: { 
+                "content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        }).then((res) => res.json())
+
+        console.log(result)
+
+        if(username == ''){ 
+            setErrorusername("Username can't be Empty")
+        }else{ 
+            setErrorusername("")
+        }
+
+        if(password.length < 8){ 
+            setErrorPass("Password Must be More than 8 Characters")
+        }else { 
+            setErrorPass('')
+        }
+        
+        if(confirmPass !== password){
+            setErrconfirm('Passwords Must be Identical')
+        }else{ 
+            setErrconfirm('')
+        }
+    }
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
   return (
     <div>
+        
         <div className=' w-500 max-w-[700px] px-10 py-20 rounded-3xl bg-white border-2 border-gray-100'>
             <div className=''>
               <h1 className='text-5xl flex flex-col  font-semibold'>Register</h1>
@@ -14,41 +55,54 @@ const UserRegister = () => {
             </div>
         
         <div className='mt-8'>
+        <form onSubmit={handleSubmit}>
             <div className=' flex-col'>
-                <label className='text-lg font-medium'>Username</label>
+                <label className='text-lg text-gray-600 font-medium'>Username</label>
                 <input 
                     className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                     placeholder="Enter your Username"/>
+                    <p className='text-red-500'>{errorusername}</p>
             </div>
             <div className=' flex-col mt-4'>
-                <label className='text-lg font-medium'>Password</label>
+                <label className='text-lg text-gray-600 font-medium'>Password</label>
                 <input 
                     className='w-full  border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
                     placeholder="Enter your Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
                     type={"password"}
                 />
+                <p className='text-red-500'>{errorPass}</p>
             </div>
             <div className=' flex-col mt-4'>
-                <label className='text-lg font-medium'>Confirm Password</label>
+                <label className='text-lg text-gray-600 font-medium'>Confirm Password</label>
                 <input 
                     className='w-full  border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-                    placeholder="Enter your Password"
+                    placeholder="Confirm your Password"
+                    onChange={(e) => setConfirmPass(e.target.value)}
+                    value={confirmPass}
                     type={"password"}
                 />
+                <p className='text-red-500'>{errconfirm}</p>
             </div>
             <div className='mt-8 flex flex-col gap-y-4'>
 
-                    <Button value="Sign up" type="submit" />
+                    <Button value="Sign up"/>
             </div>
             <div className='mt-8 flex justify-center items-center'>
                 <p className='font-medium mr-1 text-base'>Already Have an Account?</p>
                
                 <a href='/' className="text-blue-700 font-semibold hover:text-blue-300 hover:cursor-pointer"> Sign In</a>
             </div>
+            </form>
         </div>
     </div>
     </div>
   )
+
+  
 }
 
 export default UserRegister
